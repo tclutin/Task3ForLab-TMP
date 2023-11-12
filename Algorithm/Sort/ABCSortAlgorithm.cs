e15cb2662babe758e49010f86f669e71d0360854
Lutin <tclutin01@gmail.com>
2023-11-12 18:31:49 +0500

    fix

---
+++ Algorithm/Sort/ABCSortAlgorithm.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4.Algorithm.Sort
+{
+    public class ABCSortAlgorithm : IAlgorithm<string>
+    {
+        public void Execute(string[] array)
+        {
+            array = ABCSort(array, 0);
+        }
+
+        public string[] ABCSort(string[] array, int rank = 0)
+        {
+            if (array.Length < 2)
+                return array;
+
+            var table = new Dictionary<char, List<string>>();
+            var shortWordsCounter = 0;
+
+            foreach (var str in array)
+            {
+                if (rank < str.Length)
+                {
+                    var key = char.ToUpper(str[rank]);
+                    if (table.ContainsKey(key))
+                        table[key].Add(str);
+                    else
+                        table.Add(key, new List<string> { str });
+                }
+                else
+                {
+                    shortWordsCounter++;
+                }
+            }
+
+            int index = 0;
+            for (var i = 'A'; i <= 'Z'; i++)
+            {
+                if (table.ContainsKey(i))
+                {
+                    foreach (var str in ABCSort(table[i].ToArray(), rank + 1))
+                    {
+                        array[index++] = str;
+                    }
+                }
+            }
+
+            return array;
+        }
+    }
+}
+
+
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using Lab4.Algorithm;
 using Lab4.Algorithm.Sort;
+using System.Linq.Expressions;
 
 namespace Lab4
 {
@@ -8,19 +9,14 @@ namespace Lab4
 
         public static void Main(string[] args)
         {
-            
+
             AlgorithmProfiler profiler = new AlgorithmProfiler();
             string[] words = "BUGAGA AR CQ HD DB DA DB BUGAGA".Split(new[] { ' ', '.', ',', '!', '?' }, StringSplitOptions.RemoveEmptyEntries);
             //string[] words1 = "BUGAGA AR CQ HD DB DA DB BUGAGA".Split(new[] { ' ', '.', ',', '!', '?' }, StringSplitOptions.RemoveEmptyEntries);
-
-
             var x = profiler.RunExtra(words, new InsertionSortAlgorithm());
-
-            Console.WriteLine(x.Name);
-            Console.WriteLine(x.Lenght);
-            Console.WriteLine(x.Time);
             x.PrintSorted();
             x.PrintWordsOfCount();
+
 
 
         }
